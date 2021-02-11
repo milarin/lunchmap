1221780a5b33c7398a76d428337aac70b4d28422
milan <acmilanpooh@gmail.com>
2021-02-11 15:54:31 +0900

    トップページ制作

--- lunchmap/routes/web.php
+++ lunchmap/routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/shops', 'ShopController@index')->name('shop.list');
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/shops');
 });
