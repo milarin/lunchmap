0bf9d368d643336a5d1d14696882f608ccfc75e5
milan <acmilanpooh@gmail.com>
2021-02-12 15:04:54 +0900

    新規投稿画面追加

--- lunchmap/app/Http/Controllers/ShopController.php
+++ lunchmap/app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Shop;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
@@ -25,7 +26,8 @@ class ShopController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all()->pluck('name', 'id');
+        return view('new', ['categories' => $categories]);
     }
 
     /**
@@ -36,7 +38,12 @@ class ShopController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $shop = new Shop;
+        $shop->name = request('name');
+        $shop->address = request('address');
+        $shop->category_id = request('category_id');
+        $shop->save();
+        return redirect()->route('shop.detail', ['id' => $shop->id]);
     }
 
     /**
--- lunchmap/routes/web.php
+++ lunchmap/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/shops', 'ShopController@index')->name('shop.list');
+Route::get('/shop/new', 'ShopController@create')->name('shop.new');
+Route::post('/shop', 'ShopController@store')->name('shop.store');
+
 Route::get('/shop/{id}', 'ShopController@show')->name('shop.detail');
 
 Route::get('/', function () {
